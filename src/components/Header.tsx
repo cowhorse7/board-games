4c849881d8200ac73f47de6beb6cb5deb6cde4b6
cowhorse7 <becca11175@gmail.com>
2026-03-19 19:03:57 -0600

    it's nasty but not broken

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
+import "./Header.css";
 export default function Header() {
   return (
-    <div>
-      <h1>Board Games</h1>
-      <p className="profile">Profile</p>
-    </div>
+    <header className="header">
+      <div className="heading-container">
+        <h1>Board Games</h1>
+        <p className="profile">Profile</p>
+      </div>
+    </header>
   );
 }
